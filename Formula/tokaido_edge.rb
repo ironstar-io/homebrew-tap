5e6a052c89f321b6c974953195ea0c43e99f969e
otakumike <otakumike@users.noreply.github.com>
2018-08-04 15:00:23 +1000

    Add Tokaido Edge formula

--- Formula/tokaido_edge.rb
+++ Formula/tokaido_edge.rb
@@ -1,5 +1,5 @@
-class Tokaido < Formula
-    desc "Zero-conf Drupal Environments using Docker - Latest Unstable Build Version"
+class Tokaido_Edge < Formula
+    desc "Zero-conf Drupal Environments using Docker"
     homepage "https://tokaido.io"
     url "https://github.com/ironstar-io/tokaido/releases/download/edge/tok-macos"
     sha256 "90dca4b80342cd49e26eb78a24c927a2c3a982096a45a854b5090fff7e23a450"
@@ -9,6 +9,6 @@ class Tokaido < Formula
     bottle :unneeded
   
     def install
-      bin.install "tok-edge"
+      bin.install "tok"
     end
-  end
+end
