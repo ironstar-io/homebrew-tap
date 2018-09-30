be8a5f581575e80335d63a5e7638053cd05c7959
otakumike <otakumike@users.noreply.github.com>
2018-09-30 14:25:25 +1000

    1.2.0

--- Formula/tokaido.rb
+++ Formula/tokaido.rb
@@ -1,9 +1,9 @@
 class Tokaido < Formula
     desc "Zero-conf Drupal Environments using Docker"
     homepage "https://tokaido.io"
-    url "https://github.com/ironstar-io/tokaido/releases/download/1.1.2/tok-macos"
-    version "1.1.2"
-    sha256 "5fac01abf8d4ed257c9d11e6ea608f41337d74d410e769493348b2d28e69783b"
+    url "https://github.com/ironstar-io/tokaido/releases/download/1.2.0/tok-macos"
+    version "1.2.0"
+    sha256 "91525e589cbb96273067445e79f6a434a646a6159c1cf9788230f3c7163d8362"
 
     depends_on "unison"
 
