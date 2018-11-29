3485434c05b49a6f134150ac81e72f4e2cb98da3
otakumike <otakumike@users.noreply.github.com>
2018-11-30 09:35:58 +1100

    1.4.2 release

--- Formula/tokaido.rb
+++ Formula/tokaido.rb
@@ -1,7 +1,7 @@
 class Tokaido < Formula
     desc "Zero-conf Drupal Environments using Docker"
     homepage "https://tokaido.io"
-    url "https://github.com/ironstar-io/tokaido/releases/download/1.4.1/tok-macos"
+    url "https://github.com/ironstar-io/tokaido/releases/download/1.4.2/tok-macos"
     version "1.4.2"
     sha256 "0bd3ff55c6730b7438fd3852f21b268717572bd6db07cf6306443ae8b7a6962b"
 
