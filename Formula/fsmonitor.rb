f30131873889b7a53017310fff14003af73faa5a
Mike Richardson <otakumike@users.noreply.github.com>
2019-01-17 15:56:06 +1100

    add initial fsmonitor link

---
+++ Formula/fsmonitor.rb
@@ -0,0 +1,16 @@
+class Tokaido < Formula
+    desc "Zero-conf Drupal Environments using Docker"
+    homepage "https://tokaido.io"
+    url "https://github.com/ironstar-io/unison-fsmonitor/releases/download/0.0.1/unison-fsmonitor.py"
+    version "0.0.1"
+    sha256 "798538a19fba721d256122b3bff0681b9d0cf37f897cca39458ed689abf78a08"
+
+    depends_on "unison"
+
+    bottle :unneeded
+
+    def install
+      bin.install "unison-fsmonitor.py"
+      bin.install_symlink "unison-fsmonitor.py" => "unison-fsmonitor"
+    end
+end
