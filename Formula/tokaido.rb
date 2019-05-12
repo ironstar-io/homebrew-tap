9e52eae06b17914110e841f9adc1e511f72254ec
Mike Richardson <otakumike@users.noreply.github.com>
2019-05-13 07:45:11 +1000

    release 1.8.0

--- Formula/tokaido.rb
+++ Formula/tokaido.rb
@@ -1,9 +1,9 @@
 class Tokaido < Formula
     desc "Zero-conf Drupal Environments using Docker"
     homepage "https://tokaido.io"
-    url "https://github.com/ironstar-io/tokaido/releases/download/1.7.1/tok-macos"
-    version "1.7.1"
-    sha256 "da894a7552fd8a6b8f8b143f462fb034ce69d2ad3c769cbe9830e9737a1b596e"
+    url "https://github.com/ironstar-io/tokaido/releases/download/1.8.0/tok-macos"
+    version "1.8.0"
+    sha256 "2262098312e7045d2029c53b1ab7513eaa5469d575a6619c4d078be0c82e36b8"
 
     bottle :unneeded
 
