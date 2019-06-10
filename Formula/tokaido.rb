6b2b7f4ad31aefe4ac8d721efb4a54ebb0cf02fe
Mike Richardson <otakumike@users.noreply.github.com>
2019-06-10 10:54:48 +1000

    formal 1.9 release

--- Formula/tokaido.rb
+++ Formula/tokaido.rb
@@ -1,14 +1,14 @@
 class Tokaido < Formula
-    desc "Zero-conf Drupal Environments using Docker"
-    homepage "https://tokaido.io"
-    url "https://github.com/ironstar-io/tokaido/releases/download/1.8.0/tok-macos"
-    version "1.8.0"
-    sha256 "2262098312e7045d2029c53b1ab7513eaa5469d575a6619c4d078be0c82e36b8"
+  desc "Zero-conf Drupal Environments using Docker"
+  homepage "https://tokaido.io"
+  url "https://github.com/ironstar-io/tokaido/releases/download/1.9.0/tok-macos"
+  version "1.9.0"
+  sha256 "1abd4113f54d645fa857242770a3f2ae5d971e12248d743ccdcb19939bd124a9"
 
-    bottle :unneeded
+  bottle :unneeded
 
-    def install
-      bin.install "tok-macos"
-      bin.install_symlink "tok-macos" => "tok"
-    end
+  def install
+    bin.install "tok-macos"
+    bin.install_symlink "tok-macos" => "tok"
+  end
 end
--- Formula/tokaido@1.9.rb
+++ Formula/tokaido@1.9.rb
@@ -1,14 +1,14 @@
 class TokaidoAT19 < Formula
-    desc "Zero-conf Drupal Environments using Docker"
-    homepage "https://tokaido.io"
-    url "https://github.com/ironstar-io/tokaido/releases/download/1.9.0-rc1/tok-macos"
-    version "1.9.0-rc1"
-    sha256 "aab92d5260a53d30718c24cf1d0d76390560e1cc28bed1bcb4e73d42a811c07f"
+  desc "Zero-conf Drupal Environments using Docker"
+  homepage "https://tokaido.io"
+  url "https://github.com/ironstar-io/tokaido/releases/download/1.9.0/tok-macos"
+  version "1.9.0"
+  sha256 "1abd4113f54d645fa857242770a3f2ae5d971e12248d743ccdcb19939bd124a9"
 
-    bottle :unneeded
+  bottle :unneeded
 
-    def install
-      bin.install "tok-macos"
-      bin.install_symlink "tok-macos" => "tok"
-    end
+  def install
+    bin.install "tok-macos"
+    bin.install_symlink "tok-macos" => "tok"
   end
+end
